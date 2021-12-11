485f0dab5d04cfb4b068f887d1db484df1f51b5a
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2021-12-11 18:35:55 +0100

    Add Basilisk V3 scroll operations to daemon

--- daemon/openrazer_daemon/hardware/mouse.py
+++ daemon/openrazer_daemon/hardware/mouse.py
@@ -2356,6 +2356,10 @@ class RazerBasiliskV3(__RazerDeviceSpecialBrightnessSuspend):
                'get_brightness', 'set_brightness',
                'get_logo_brightness', 'set_logo_brightness',
                'get_scroll_brightness', 'set_scroll_brightness',
+               # Basilisk V3 scroll wheel controls
+               'get_scroll_mode_basilisk_v3', 'set_scroll_mode_basilisk_v3',
+               'get_scroll_acceleration_basilisk_v3', 'set_scroll_acceleration_basilisk_v3',
+               'get_scroll_smart_reel_basilisk_v3', 'set_scroll_smart_reel_basilisk_v3',
                # All LEDs (partial support)
                'set_static_effect', 'set_wave_effect', 'set_spectrum_effect', 'set_none_effect',
                # Logo (partial support)
